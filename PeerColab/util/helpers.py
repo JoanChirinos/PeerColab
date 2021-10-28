e9c76ea1b2826ea189e77bc27667f381709a9d92
Joan Chirinos <jc2624@cornell.edu>
2021-10-28 03:54:26 -0400

    Create helper library
    Will house helper functions that don't need flask, db, etc.
    Currently houses verify_auth_args
    
    TODO: robustify verify_auth_args

---
+++ PeerColab/util/helpers.py
@@ -0,0 +1,30 @@
+"""
+PeerColab
+
+Helper functions for the main Flask file
+
+Copyright Joan Chirinos, 2021.
+"""
+
+
+def verify_auth_args(*args: str) -> bool:
+    """
+    Verify arguments from user-inputted authentication forms
+
+    Parameters
+    ----------
+    *args : str
+        The non-keyworded arguments to verify
+
+    Returns
+    -------
+    bool
+        True if all form elemens are valid.
+        False otherwise.
+
+    """
+    # TODO: Robustify. Possibly using **kwargs and arg-specific verification
+    for arg in args:
+        if len(arg.strip()) == 0 or arg.strip() != arg:
+            return False
+    return True
